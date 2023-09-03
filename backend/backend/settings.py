0912d45b8baa1d040cba55a08a481360be597ed1
tania <tania_maika@list.ru>
2023-09-03 19:24:53 +0000

    First commit taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,10 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['51.250.21.23', '127.0.0.1', 'localhost', 'taski.ivanova-tanya.site']
 
 # Application definition
 
